e73794c2f329c8997087b9f026d0eed69d9a9881
Jalaj <jalaj95@gmail.com>
2018-04-18 00:02:33 +0530

    database commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :patients
-  get '/patients/search', to: "patients#search"
+  root to: "patients#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
